8362e85f3fca03f0907e743c4e849774a6b0456f
erramifadelellah && ayman eljamaaoui <fadelellaherrami@gmail.com>
2023-02-09 03:15:44 +0100

    handle directory in php

--- files/index.php
+++ files/index.php
@@ -1,11 +1,11 @@
 <?php 
     // $names = readfile('context.txt');
     // echo $names;
-    $file = "context.txt";
+    // $file = "context.txt";
 
-    if(file_exists($file)){
+    // if(file_exists($file)){
         //$handle = fopen($file,'r+');// read and write
-        $handle = fopen($file,"a+");// read and write (position in the end)
+        // $handle = fopen($file,"a+");// read and write (position in the end)
         // echo fread($handle,filesize($file)/2);
         // read one line
         // echo fgets($handle);
@@ -16,8 +16,8 @@
         //
         // get one charactere
         // echo fgetc($handle);
-        fwrite($handle,"\n LAHOUCINE ERRAMI");
-        echo fread($handle, filesize($file));
+        // fwrite($handle,"\n LAHOUCINE ERRAMI");
+        // echo fread($handle, filesize($file));
         // echo readfile($file);
         // //
         // echo filesize($file);
@@ -29,6 +29,14 @@
         // rename('data.txt','my_file.txt');
         // delete file
         // unlink("my_file.txt");
-        fclose($handle);
-    }else echo "file doesn't exist";
+        // fclose($handle);
+    // }else echo "file doesn't exist";
+
+    // create a directoly 
+    mkdir('storage');
+    // create a file
+    touch("storage/index.txt")
+    // remove directoly 
+    //rmdir('storage');
+    // when a directoly isn't empty we can't remove it with rmdir we use unlink() method
 ?>
