be9a1430c208cf5d3ae2d4d597ec67b72a70b5f4
erramifadelellah && ayman eljamaaoui <fadelellaherrami@gmail.com>
2023-02-09 02:59:32 +0100

    handle file for programming

--- files/index.php
+++ files/index.php
@@ -2,17 +2,33 @@
     // $names = readfile('context.txt');
     // echo $names;
     $file = "context.txt";
+
     if(file_exists($file)){
-        echo readfile($file);
+        //$handle = fopen($file,'r+');// read and write
+        $handle = fopen($file,"a+");// read and write (position in the end)
+        // echo fread($handle,filesize($file)/2);
+        // read one line
+        // echo fgets($handle);
+        // 
+        // while(!feof($handle)){
+        //     echo fgets($handle);
+        // }
         //
-        echo filesize($file);
-        //
-        echo realpath($file);
-        //
+        // get one charactere
+        // echo fgetc($handle);
+        fwrite($handle,"\n LAHOUCINE ERRAMI");
+        echo fread($handle, filesize($file));
+        // echo readfile($file);
+        // //
+        // echo filesize($file);
+        // //
+        // echo realpath($file);
+        // //
         // copy($file,'data.txt');
         //
         // rename('data.txt','my_file.txt');
         // delete file
-        unlink("my_file.txt");
+        // unlink("my_file.txt");
+        fclose($handle);
     }else echo "file doesn't exist";
 ?>
