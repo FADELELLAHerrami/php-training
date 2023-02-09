df36432f1764a5992ddb9d5c6d0f8cfbb4695a1e
erramifadelellah && ayman eljamaaoui <fadelellaherrami@gmail.com>
2023-02-09 02:33:38 +0100

    copy, rename and delete file in php

--- files/index.php
+++ files/index.php
@@ -8,5 +8,11 @@
         echo filesize($file);
         //
         echo realpath($file);
+        //
+        // copy($file,'data.txt');
+        //
+        // rename('data.txt','my_file.txt');
+        // delete file
+        unlink("my_file.txt");
     }else echo "file doesn't exist";
 ?>
